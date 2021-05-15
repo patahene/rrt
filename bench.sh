4fd1c3f1b1dd1c0c79ff514742f16f790c79ff5f
Katsuya Tanabe <patahene@gmail.com>
2021-05-15 18:41:47 +0900

    add bench.sh

---
+++ bench.sh
@@ -0,0 +1,12 @@
+./target/release/rrt -t=1
+./target/release/rrt -t=2
+./target/release/rrt -t=3
+./target/release/rrt -t=4
+./target/release/rrt -t=5
+./target/release/rrt -t=6
+./target/release/rrt -t=7
+./target/release/rrt -t=8
+./target/release/rrt -t=9
+./target/release/rrt -t=10
+./target/release/rrt -t=11
+./target/release/rrt -t=12
